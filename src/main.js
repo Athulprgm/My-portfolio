66327bf02777a62bd88674e885b13c8a086f424d
Athul Krishna K <athulprgm.pycslearn@gmail.com>
2026-09-16 10:50:14 +0530

    update portfolio

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './index.css';
-import './App.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
-createApp(App).mount('#app');
